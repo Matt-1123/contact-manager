459918553e583b96c8a51df047840753f8af1620
Matt-1123 <matthew.russo91@gmail.com>
2019-02-08 15:36:17 -0500

    add Header functional component and Contact class component

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+  render() {
+    const { name, email, phone } = this.props;
+    return (
+      <div>
+        <h4>{name}</h4>
+        <ul>
+          <li>{email}</li>
+          <li>{phone}</li>
+        </ul>
+      </div>
+    );
+  }
+}
