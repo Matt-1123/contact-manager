f59c8ef38d07f49b41667481a40d7df31487b0a0
Matt-1123 <matthew.russo91@gmail.com>
2019-02-13 09:57:26 -0500

    add delete buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,10 @@ class Contact extends Component {
     showContactInfo: false
   };
 
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  }
+
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
@@ -15,9 +19,17 @@ class Contact extends Component {
         <h4>
           {name}
           <i
-            onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
-            className="fas fa-sort-down">
-          </i>
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+            style={{ cursor: 'pointer' }}
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+            onClick={this.onDeleteClick}
+          />
         </h4>
         {showContactInfo ? (
           <ul className="list-group">
@@ -31,7 +43,8 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isrequired
 };
 
 export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -25,6 +25,16 @@ class Contacts extends Component {
     ]
   };
 
+  deleteContact = (id) => {
+    const { contacts } = this.state;
+
+    const newContacts = contacts.filter(contact => contact.id !== id);
+
+    this.setState({
+      contacts: newContacts
+    });
+  }
+
   render() {
     const { contacts } = this.state;
 
@@ -33,7 +43,8 @@ class Contacts extends Component {
         {contacts.map(contact => (
           <Contact
             key={contact.id}
-            contact={contact} //pass in entire object
+            contact={contact} //pass in contact prop
+            deleteClickHandler={this.deleteContact.bind(this, contact.id)} //pass in deleteClickHandler prop
           />
         ))}
       </React.Fragment>
